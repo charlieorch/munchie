a10e89d97b15554db16d1c3c046bb889d42e5562
charlieorch <charlieorch1@gmail.com>
2021-03-31 15:34:24 -0500

    replaced default React render to display desired components in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+      <h1>Ravenous</h1>
+      <SearchBar />
+      <BusinessList /> 
     </div>
   );
 }
